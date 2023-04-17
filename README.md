3406cf7b47d17307f252268fa37bc2b9f44d685a
sami0web <79028718+sami0web@users.noreply.github.com>
2023-04-17 14:01:04 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-17-4-2023-3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-71fkqr)
---
+++ src/components/Newtab.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './Newtab.css';
+
+import { useSelector } from 'react-redux';
+
+function Newtab() {
+ 
+
+ let user = useSelector(state => state);
+
+ 
+console.log(user)
+
+  return (
+
+<div>
+
+gggggggggggggggg
+
+
+
+
+
+gjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+
+
+
+
+
+
+</div>
+    
+  )
+
+
+
+
+
+
+
+}
+
+
+
+export default Newtab;
